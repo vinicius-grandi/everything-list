f293877138e555116a010f7145caf5931e2f84c2
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-26 13:44:41 -0300

    removing unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import OneSignal from 'react-onesignal';
 import * as api from './services/jikanapi';
 
 function App() {
   const [subscription, setSubscription] = useState(false);
   useEffect(() => {
-    OneSignal.getSubscription().then(
-      (res) => setSubscription(res),
-    );
     api.getSchedule(7).then((r) => console.log(r));
   }, []);
   return (
