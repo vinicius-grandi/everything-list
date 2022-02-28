39e29b9c806fe20b768f533ed7c5ac6aacbc071f
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-02-28 10:41:07 -0300

    setup is done... again

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './index.css';
 import Home from './views/Home';
 
 const App = (): JSX.Element => (
--- myapp/src/views/Home.tsx
+++ myapp/src/views/Home.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-const Home = () => {
+const Home = (): JSX.Element => {
   useEffect(() => {
     fetch('/message')
-      .then((res) => console.log(res));
-  })
+      .then((res) => res)
+      .then((data) => console.log(data.text().then((r) => console.log(r))));
+  });
 
-  return <h1>Home</h1>
+  return <h1>Home</h1>;
 };
 
 export default Home;
