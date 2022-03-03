a21052755acfbacfe989fb3767f4e01f0300d208
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-03-03 18:32:58 -0300

    brb after reviewing generic types

--- express-gen-ts/src/app/models/User.ts
+++ express-gen-ts/src/app/models/User.ts
@@ -2,16 +2,26 @@ import {
   Model,
   Sequelize,
   DataTypes,
+  ModelStatic,
+  ModelAttributes,
+  Attributes,
 } from 'sequelize';
+
 class User extends Model {
   declare id: number;
   declare username: string;
   declare pasword_hash: string;
 
-  static init(sequelize) {
-    super.init({
-      username: DataTypes.STRING
-    }, sequelize)
+  public static init(sequelize: Sequelize) {
+    const attributes = {
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.VIRTUAL,
+    };
+
+    return this.init(attributes, {
+        sequelize
+    });
   }
 }
 
