77c1e4ad17fbf2e12465cc3a818dbc8678bebdec
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-03-28 21:40:39 -0300

    proxying

--- myapp/src/setupProxy.js
+++ myapp/src/setupProxy.js
@@ -1,13 +1,14 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
+/** @type {import('http-proxy-middleware').Options} */
 const proxy = {
   target: 'http://localhost:5001',
   changeOrigin: true,
-}
+};
 
 module.exports = (app) => {
   app.use(
-    '/',
+    '/search',
     createProxyMiddleware(proxy),
   );
 };
