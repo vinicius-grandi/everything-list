4b087b5de74187846cb7ba9e3a5809524284d389
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-04-22 14:20:18 -0300

    pagination

--- server/src/app/controllers/NoApiController.ts
+++ server/src/app/controllers/NoApiController.ts
@@ -2,13 +2,35 @@ import { Request, Response } from 'express';
 import getModelName from '../../utils/db/getModelName';
 import db from '../models';
 
-const NoApiController = {
-  async getAllItems(req: Request, res: Response) {
-    const { baseUrl } = req;
-    const modelName = getModelName(baseUrl);
-    const items = await db[modelName].findAll();
+interface IQueryParams {
+  page: 'string';
+}
 
-    res.json(items);
+const NoApiController = {
+  async getAllItems(
+    req: Request<unknown, unknown, unknown, IQueryParams>,
+    res: Response,
+  ) {
+    const { baseUrl } = req;
+    const page = req.query.page === undefined ? 1 : Number(req.query.page);
+    const modelName = getModelName(baseUrl);
+
+    const lastPage = Math.ceil((await db[modelName].count()) / 20);
+    const items = await db[modelName].findAll({
+      limit: 20,
+      offset: page === 1 ? 0 : (page - 1) * 20,
+    });
+
+    if (!items) {
+      return res.status(404).json({ error: 'no items' });
+    }
+
+    return res.json({
+      pagination: {
+        lastVisiblePage: lastPage,
+      },
+      data: items,
+    });
   },
 };
 
