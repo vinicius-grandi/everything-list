106b9119daa3a9ace58ea5d023ded67920504b38
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-05-02 16:12:34 -0300

    moving files and creating a true monorepo

--- packages/client/src/views/animes/AnimeDetails.tsx
+++ packages/client/src/views/animes/AnimeDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import type { Animes } from '@everything-list/server/src/services/animes/jikanapi.d';
 
 function AnimeDetails(): JSX.Element {
   return <h1>Animes</h1>;
