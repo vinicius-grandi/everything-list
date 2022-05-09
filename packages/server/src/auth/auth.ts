8ce5b6a853bac0d3320c9c6bb83a405133c00678
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-05-09 20:59:46 -0300

    .

--- packages/server/src/auth/auth.ts
+++ packages/server/src/auth/auth.ts
@@ -1,11 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
-import db from '../app/models';
-
-const { User } = db;
 
 export default async (req: Request, res: Response, next: NextFunction) => {
-  const user = await User.findByPk(req.session.userId);
-  if (!user) {
+  if (!req.session.authenticated) {
     return res.status(401).json({ error: 'not authorized.' });
   }
   return next();
