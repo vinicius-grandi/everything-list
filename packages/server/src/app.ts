98f447c5844c820405ba8f6c225c09ee6e331cdd
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-05-09 18:57:34 -0300

    allowing any site in content security policy img-src

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -41,7 +41,31 @@ const session = sessions({
 // handling upload
 const fileSize = 50 * 1024 * 1024;
 
-app.use(helmet());
+// helmet configs
+app.use(helmet.crossOriginEmbedderPolicy());
+app.use(helmet.crossOriginOpenerPolicy());
+app.use(helmet.crossOriginResourcePolicy());
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.originAgentCluster());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        imgSrc: ['https://*'],
+      },
+    },
+  }),
+);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(session);
