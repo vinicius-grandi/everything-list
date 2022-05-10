f1eea47ee8e65c03d3edebb29f7ad0b85a936441
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-05-09 21:00:46 -0300

    .

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -88,7 +88,7 @@ app.use(searchRouter);
 app.use(noApiListRoutes, noApiListRouter);
 app.use(apiListRoutes, apiListRouter);
 app.use('/profiles', userRouter);
-app.use('/api', authRouter);
 app.use('/', express.static(resolve('..', 'client', 'build')));
+app.use('/api', authRouter);
 
 export default app;
