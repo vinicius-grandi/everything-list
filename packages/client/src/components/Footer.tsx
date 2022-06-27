7baaa5441fefdac6b2b36f789292d273de47af9e
Vinícius Gustavo Grandi <93281527+vinicius-grandi@users.noreply.github.com>
2022-06-27 19:01:41 -0300

    .

--- packages/client/src/components/Footer.tsx
+++ packages/client/src/components/Footer.tsx
@@ -5,8 +5,6 @@ const StyledFooter = styled.footer`
   background-color: #543275;
   color: #f6f6f6;
   padding: 0.15rem;
-  position: absolute;
-  bottom: 0%;
   width: 100%;
   padding: 0.5rem;
   box-sizing: border-box;
